95119d4c0b7e4d627319fdbfa328370b51cf0462
s-traut <stephane.traut@gmail.com>
2022-05-21 13:04:14 +0200

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-const utils = require('./src/utils.js');
-const texture = require('./src/texture.js');
-
-exports.dec2hexString = utils.dec2hexString;
-exports.getFont = texture.getFont;
-exports.generateTexture = texture.generateTexture;
--- src/utils.js
+++
@@ -1,5 +0,0 @@
-function dec2hexString(dec, x = 4) {
-  return (dec+0x10000).toString(16).substr(-x).toUpperCase();
-}
-
-module.exports = { dec2hexString };
